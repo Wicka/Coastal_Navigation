071f119a896f014634c004e417be33b7cd9c051b
Wicka <mesaes@gmail.com>
2020-09-25 10:29:47 +0200

    Modified User

--- src/main/java/wicka/coast/navigation/dto/User.java
+++ src/main/java/wicka/coast/navigation/dto/User.java
@@ -14,7 +14,10 @@ import wicka.coast.navigation.dto.type.TypeUser;
 @Entity
 @Table(name="user")
 public class User {
-	 @Id
+	
+
+
+		@Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id; // Id User Auto-Generated
 
@@ -27,4 +30,40 @@ public class User {
 
 	    @Column(name="password")
 	    private String password; // User Password
+	    
+		public User() {	}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public TypeUser getTypeUser() {
+			return typeUser;
+		}
+
+		public void setTypeUser(TypeUser typeUser) {
+			this.typeUser = typeUser;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		 
+		 
 }
