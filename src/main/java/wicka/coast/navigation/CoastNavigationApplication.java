e6dad415c7f41d950ef13076e3ffe5d6033b4759
Wicka <mesaes@gmail.com>
2020-09-25 09:47:09 +0200

    New Project

---
+++ src/main/java/wicka/coast/navigation/CoastNavigationApplication.java
@@ -0,0 +1,13 @@
+package wicka.coast.navigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoastNavigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoastNavigationApplication.class, args);
+	}
+
+}
